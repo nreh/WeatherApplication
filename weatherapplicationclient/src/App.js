ce1cc5af72c5ddb046681c625c263fe997c52c54
Naqeeb Rehman <rnqeeb@yahoo.com>
2021-11-19 21:56:49 -0500

    Created skeleton of home page that makes requests to backend

--- weatherapplicationclient/src/App.js
+++ weatherapplicationclient/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import { useRef } from 'react';
+
 
 function App() {
+  // reference output text
+  const outputText = useRef(null);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Weather Application
+      </h1>
+      <input onKeyPress={(e)=>searchCity(e)} />
+      <p style={{marginTop: '2px', color: 'gray'}}>Enter a city and press ENTER</p>
+      <span ref={outputText}/>
     </div>
   );
+
+  function searchCity(e) {
+    if (e.key === 'Enter') {
+      let city = e.target.value;
+  
+      // clear textbox
+      e.target.value = '';
+  
+      // make a request to server:
+      var req = new XMLHttpRequest();
+      req.open('GET', `/api/weather/city?city=${city}`, false);
+      req.send(null);
+      console.log(req.responseText);
+  
+      outputText.current.innerText = req.responseText;
+    }
+  }
 }
 
 export default App;
