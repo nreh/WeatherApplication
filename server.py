3a48765a5702148d66279811b3ee34f4db25bb58
Naqeeb Rehman <rnqeeb@yahoo.com>
2021-11-20 19:57:23 -0500

    Added ssl_context adhoc to server.py

--- server.py
+++ server.py
@@ -107,4 +107,4 @@ def page_not_found(e):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(ssl_context='adhoc',debug=True, host='0.0.0.0', port=port)
