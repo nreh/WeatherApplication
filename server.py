e3a4be627e95c16dd00f4a93f866d172d61929e9
Naqeeb Rehman <rnqeeb@yahoo.com>
2021-11-20 21:07:48 -0500

    Implemented database querying for counter

--- server.py
+++ server.py
@@ -2,21 +2,63 @@
 WeatherApplication Server power by OpenWeatherMap
 Naqeeb Rehman - 11/19/2021
 """
+from enum import unique
 import os # for environment variables
 import json # for parsing json (not the same as flask.jsonify)
 from flask import Flask
+
+from flask_sqlalchemy import SQLAlchemy # for database
+
 from flask import jsonify # for converting dict to json strings
 from flask import request
 from flask.helpers import make_response, send_from_directory # for parsing GET requests
 import requests
+
+app = Flask(__name__, static_folder='./weatherapplicationclient/build')
+
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
+if DATABASE_URL == None:
+    print("No database")
+else:
+    if DATABASE_URL.startswith('postgres://'):
+        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# class used w/ database
+class counter(db.Model):
+    city = db.Column(db.String(80), primary_key=True, nullable=False)
+    count = db.Column(db.Integer, unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<City %r>' & self.city
+
+if DATABASE_URL != None:
+    db.create_all() # initialize database
 
 # try obtaining api key:
 API_KEY = os.environ.get('OPENWEATHERMAP_APIKEY')
 
 def get_counter_for(city):
     """Retrieves from database, number of times given city was searched"""
-    #todo: implement this
-    return 10
+    if DATABASE_URL == None:
+        return ''
+    co = counter.query.filter_by(city=city).first()
+    if co is None:
+        co = counter(city=city, count=0)
+        db.session.add(co)
+        db.session.commit()
+    
+    return getattr(co, counter.count.key)
+
+def increment_counter_for(city):
+    co = counter.query.filter_by(city=city).first()
+    co.count += 1
+    db.session.commit()
+
 
 def get_weather_for(city):
     """
@@ -32,7 +74,6 @@ def get_weather_for(city):
 
     return requests.get('https://api.openweathermap.org/data/2.5/weather', params=params)
 
-app = Flask(__name__, static_folder='./weatherapplicationclient/build')
 
 # handle requests for the weather of a city
 @app.route('/api/weather/city', methods=['GET', 'OPTIONS'])
@@ -65,7 +106,10 @@ def api_weather_city():
             response.headers.add('Access-Control-Allow-Origin', '*')
             return response, 400
 
-        #* Client sent a valid request,
+        #*******************************
+        #* Client sent a valid request *
+        #*******************************
+
         # forward response from OpenWeatherMap and status to client
         result = get_weather_for(data['city'])
 
@@ -84,6 +128,8 @@ def api_weather_city():
             # OpenWeatherMap is in string format. Parse json string:
             parsed = json.loads(result.text)
             parsed['COUNTER'] = get_counter_for(data['city']) # add counter to json
+
+            increment_counter_for(data['city']) # also increment that counter
 
             response = make_response()
             response.data = json.dumps(parsed)
